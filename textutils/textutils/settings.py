fb09dd91a7fdf28611764452516b8f91991f1373
krishnasaroj279 <krishnasaroj279@gmail.com>
2022-12-10 22:51:56 +0530

    security

--- textutils/textutils/settings.py
+++ textutils/textutils/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k1f5v((v60e2*0=h@w)&%-g%3^%47@!$m@=uu7o%c_#qxkijiz'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY") #this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
